30f07df1a5e2bf9c5e89d323e3e0aa94a30ea129
jakubin <jakubin@gmail.com>
2016-11-17 20:52:15 +0100

    Additional unit tests for Common.

---
+++ src/BasniowaCore/Common.Tests/Cqrs/ServiceProviderHandlerResolverTests.cs
@@ -0,0 +1,61 @@
+﻿using Common.Cqrs;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Common.Tests.Cqrs
+{
+    public class ServiceProviderHandlerResolverTests
+    {
+        [Fact(DisplayName = nameof(ServiceProviderHandlerResolver) + ": Resolve() should correctly return when no dependency is registered.")]
+        public void ResolveNone()
+        {
+            var handlers = new IHandler<TestCommand>[0];
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IEnumerable<IHandler<TestCommand>>)))
+                .Returns(handlers);
+            var resolver = new ServiceProviderHandlerResolver(serviceProviderMock.Object);
+
+            var actualHandlers = resolver.Resolve<TestCommand>();
+            actualHandlers.Should().BeEmpty();
+        }
+
+        [Fact(DisplayName = nameof(ServiceProviderHandlerResolver) + ": Resolve() should correctly return when single dependency is registered.")]
+        public void ResolveSingle()
+        {
+            var handlers = new IHandler<TestCommand>[]
+            {
+                new Mock<IHandler<TestCommand>>().Object
+            };
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IEnumerable<IHandler<TestCommand>>)))
+                .Returns(handlers);
+            var resolver = new ServiceProviderHandlerResolver(serviceProviderMock.Object);
+
+            var actualHandlers = resolver.Resolve<TestCommand>();
+            actualHandlers.Should().BeEquivalentTo(handlers);
+        }
+
+        [Fact(DisplayName = nameof(ServiceProviderHandlerResolver) + ": Resolve() should correctly return when multiple dependency is registered.")]
+        public void ResolveMultiple()
+        {
+            var handlers = new IHandler<TestEvent>[]
+            {
+                new Mock<IHandler<TestEvent>>().Object,
+                new Mock<IHandler<TestEvent>>().Object
+            };
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IEnumerable<IHandler<TestEvent>>)))
+                .Returns(handlers);
+            var resolver = new ServiceProviderHandlerResolver(serviceProviderMock.Object);
+
+            var actualHandlers = resolver.Resolve<TestEvent>();
+            actualHandlers.Should().BeEquivalentTo(handlers);
+        }
+    }
+}
---
+++ src/BasniowaCore/Common.Tests/GuardTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Common.Tests
+{
+    public class GuardTests
+    {
+        [Fact(DisplayName = nameof(Guard) + ": NotNull() should not throw on not null values.")]
+        public void NotNullShouldNotThrowOnNonNull()
+        {
+            var notNull = new object();
+            Guard.NotNull(notNull, "notNull");
+        }
+
+        [Fact(DisplayName = nameof(Guard) + ": NotNull() should throw ArgumentNullException on null values.")]
+        public void NotNullShouldThrowOnNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>("someName", () => {
+                Guard.NotNull(null, "someName");
+            });
+
+            exception.Message.Should().Contain("someName");
+        }
+
+        [Theory(DisplayName = nameof(Guard) + ": GreaterOrEqual() should not throw on valid values.")]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(5, 1)]
+        public void GreaterOrEqualShouldNotThrowOnValid(int value, int minValue)
+        {
+            Guard.GreaterOrEqual(value, minValue, "someName");
+        }
+
+        [Fact(DisplayName = nameof(Guard) + ": GreaterOrEqual() should throw ArgumentOutOfRangeException on invalid values.")]
+        public void GreaterOrEqualShouldThrowOnInvalid()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>("someName", () => {
+                Guard.GreaterOrEqual(1, 2, "someName");
+            });
+            exception.Message.Should().Contain("someName");
+        }
+    }
+}
