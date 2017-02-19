93a8afac45d498ec263ddae0be5ccc8b12e8b890
jakubin <jakubin@gmail.com>
2017-02-19 21:53:43 +0100

    ImageFileTypes helper.

---
+++ src/BasniowaCore/Logic.Tests/Common/Images/ImageFileTypesTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Logic.Common.Images;
+using Xunit;
+
+namespace Logic.Tests.Common.Images
+{
+    public class ImageFileTypesTests
+    {
+        [Fact]
+        public void AllPublicFieldsShouldNotBeNullOrEmpty()
+        {
+            var fields = typeof(ImageFileTypes).GetTypeInfo()
+                .GetFields(BindingFlags.GetField | BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.FieldType == typeof(string));
+
+            foreach (var field in fields)
+            {
+                var value = (string)field.GetValue(null);
+                value.Should().NotBeNullOrEmpty(
+                    "field with file type extension should not contain empty or null value (field: {0})", field.Name);
+            }
+        }
+
+        [Theory]
+        [InlineData("jpg")]
+        [InlineData("JPG")]
+        [InlineData("jpeg")]
+        [InlineData("png")]
+        [InlineData("gif")]
+        [InlineData("bmp")]
+        [InlineData("tiff")]
+        public void IsImageFileExtension_True(string extension)
+        {
+            var actual = ImageFileTypes.ImageFileExtensions.Contains(extension);
+            actual.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("docx")]
+        [InlineData("txt")]
+        public void IsImageFileExtension_False(string extension)
+        {
+            var actual = ImageFileTypes.ImageFileExtensions.Contains(extension);
+            actual.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("jpg")]
+        [InlineData("JPG")]
+        [InlineData("jpeg")]
+        [InlineData("png")]
+        [InlineData("gif")]
+        public void IsCompressedImageFileExtension_True(string extension)
+        {
+            var actual = ImageFileTypes.CompressedImageFileExtensions.Contains(extension);
+            actual.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("bmp")]
+        [InlineData("tiff")]
+        [InlineData("txt")]
+        public void IsCompressedImageFileExtension_False(string extension)
+        {
+            var actual = ImageFileTypes.CompressedImageFileExtensions.Contains(extension);
+            actual.Should().BeFalse();
+        }
+    }
+}
---
+++ src/BasniowaCore/Logic/Common/Images/ImageFileTypes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Logic.Common.Images
+{
+    /// <summary>
+    /// Well-know image file extensions.
+    /// </summary>
+    public static class ImageFileTypes
+    {
+        /// <summary>
+        /// "jpeg" file extension.
+        /// </summary>
+        public static readonly string Jpeg = "jpeg";
+
+        /// <summary>
+        /// "jpg" file extension.
+        /// </summary>
+        public static readonly string Jpg = "jpg";
+
+        /// <summary>
+        /// "png" file extension.
+        /// </summary>
+        public static readonly string Png = "png";
+
+        /// <summary>
+        /// "gif" file extension.
+        /// </summary>
+        public static readonly string Gif = "gif";
+
+        /// <summary>
+        /// "bmp" file extension.
+        /// </summary>
+        public static readonly string Bmp = "bmp";
+
+        /// <summary>
+        /// "tiff" file extension.
+        /// </summary>
+        public static readonly string Tiff = "tiff";
+
+        /// <summary>
+        /// Initializes the <see cref="ImageFileTypes"/> class.
+        /// </summary>
+        static ImageFileTypes()
+        {
+            ImageFileExtensions = new[] {Jpeg, Jpg, Png, Gif, Bmp, Tiff}
+                .ToImmutableSortedSet(StringComparer.OrdinalIgnoreCase);
+            CompressedImageFileExtensions = new[] {Jpeg, Jpg, Png, Gif}
+                .ToImmutableSortedSet(StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Gets the file extensions representing an image type.
+        /// </summary>
+        public static ImmutableSortedSet<string> ImageFileExtensions { get; }
+
+        /// <summary>
+        /// Gets the file extensions representing a compressed image type.
+        /// </summary>
+        public static ImmutableSortedSet<string> CompressedImageFileExtensions { get; }
+    }
+}
