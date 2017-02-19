1618cdae3ff9f4bda460a80945fb20c140199ac5
jakubin <jakubin@gmail.com>
2017-02-19 22:01:33 +0100

    Partial implementation of adding show picture handler. Rule checking if image has a valid type. Command handler file template (resharper).

---
+++ src/BasniowaCore/Logic/Shows/AddShowPictureHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Cqrs;
+using Logic.Common.BusinessRules;
+using Logic.Common.Images;
+using Logic.Services;
+
+namespace Logic.Shows
+{
+    /// <summary>
+    /// Handles <see cref="AddShowPictureCommand"/>.
+    /// </summary>
+    public class AddShowPictureHandler : IHandler<AddShowPictureCommand>
+    {
+        #region Services
+
+        /// <summary>
+        /// Gets or sets the database.
+        /// </summary>
+        public IDbContextFactory DbFactory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event publisher.
+        /// </summary>
+        public IEventPublisher EventPublisher { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date time service.
+        /// </summary>
+        public IDateTimeService DateTimeService { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier service.
+        /// </summary>
+        public IUniqueIdService IdService { get; set; }
+
+        #endregion
+
+        /// <inheritdoc/>
+        public async Task Handle(AddShowPictureCommand command)
+        {
+            var extension = Path.GetExtension(command.FileName);
+            if (!ImageFileTypes.ImageFileExtensions.Contains(extension))
+            {
+                throw new BusinessRuleException<FileMustBeImageRule>(
+                    new FileMustBeImageRule(extension, ImageFileTypes.ImageFileExtensions.ToArray()));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BasniowaCore/Logic/Shows/FileMustBeImageRule.cs
@@ -0,0 +1,38 @@
+﻿using Logic.Common.BusinessRules;
+
+namespace Logic.Shows
+{
+    /// <summary>
+    /// Business rule ensuring that provided file must be an image.
+    /// </summary>
+    public sealed class FileMustBeImageRule : IBusinessRule
+    {
+        /// <inheritdoc/>
+        public string RuleId => "FileMustBeImage";
+
+        /// <summary>
+        /// Gets the file provided by the user extension.
+        /// </summary>
+        public string FileExtension { get; }
+
+        /// <summary>
+        /// Gets the allowed file extensions.
+        /// </summary>
+        public string[] AllowedFileExtensions { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileMustBeImageRule"/> class.
+        /// </summary>
+        /// <param name="fileExtension">The file extension.</param>
+        /// <param name="allowedFileExtensions">The allowed file extensions.</param>
+        public FileMustBeImageRule(string fileExtension, string[] allowedFileExtensions)
+        {
+            FileExtension = fileExtension;
+            AllowedFileExtensions = allowedFileExtensions;
+        }
+
+        /// <inheritdoc/>
+        public string GetUserMessage() =>
+            $"File must be an image. Allowed extensions are: {string.Join(", ", AllowedFileExtensions)}.";
+    }
+}
