26ca16aa2063e0f4db037fb629c9dce20a8b829e
jakubin <jakubin@gmail.com>
2017-03-03 19:58:44 +0100

    Removed Stylecop.

--- src/BasniowaCore/Website/Startup.cs
+++ src/BasniowaCore/Website/Startup.cs
@@ -17,7 +17,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Swashbuckle.AspNetCore.Swagger;
